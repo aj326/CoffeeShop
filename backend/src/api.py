6b43deda760b5742a6a585c087a54020831fe28c
aj <ahmed24633@gmail.com>
2022-06-12 21:36:11 +0300

    succeeded in using fronend to login fixed type in api.py:67

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,4 +1,7 @@
 import os
+
+import requests
+import sqlalchemy
 from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
 import json
@@ -13,6 +16,9 @@ print(app)
 print("Here i am")
 setup_db(app)
 CORS(app)
+
+
+
 
 '''
 @TODO uncomment the following line to initialize the datbase
@@ -44,18 +50,35 @@ def get_drinks():
                 "drinks": short,
             }
         )
-    except Exception:
+    except requests.HTTPError:
         abort(404)
 
 
 '''
-@TODO implement endpoint
+@DONE implement endpoint
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route("/drinks-detail")
+@requires_auth('get:drinks-detail')
+def get_drinks_with_details(permission):
+    try:
+        selection = Drink.query.order_by(Drink.id).all()
+        long = [drink.long() for drink in selection]
+        return jsonify(
+            {
+                "success": True,
+                "drinks": long,
+            }
+        )
+    except requests.HTTPError:
+        abort(404)
+
 
 '''
 @TODO implement endpoint
@@ -66,6 +89,27 @@ def get_drinks():
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route("/drinks", methods=['POST'])
+@requires_auth('post:drinks')
+def create_new_drink(permission):
+    data = request.get_json()
+    title = data.get('title', None)
+    recipe = data.get('recipe', None)
+    if not title or not recipe:
+        abort(422)
+    drink = Drink(title=title, recipe=json.dumps(recipe))
+    try:
+        drink.insert()
+        return jsonify(
+            {
+                "success": True,
+                "drinks": [drink.long()]
+            })
+    except sqlalchemy.exc.DataError:
+        abort(500)
+
 
 '''
 @TODO implement endpoint
@@ -124,6 +168,24 @@ def resource_not_found(error):
         "message": "resource not found"
     }), 404
 
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({
+        "success": False,
+        "error": 500,
+        "message": "Internal Server Error"
+    }), 500
+
+
+@app.errorhandler(AuthError)
+def auth_error(error):
+    # print("in autherror", error)
+    return jsonify({
+        "success": False,
+        "error": error.status_code,
+        "message": error.error['description']
+    }), error.status_code
+
 
 '''
 @TODO implement error handlers using the @app.errorhandler(error) decorator
@@ -142,6 +204,6 @@ def resource_not_found(error):
 '''
 
 '''
-@TODO implement error handler for AuthError
+@DONE implement error handler for AuthError
     error handler should conform to general task above
 '''
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'coffeeshop-udacity-ahmed.us.auth0.com', // the auth0 domain prefix
+    url: 'coffeeshop-udacity-ahmed.us', // the auth0 domain prefix
     audience: 'menu', // the audience set for the auth0 app
     clientId: 'DrNuZGxw0roV3Mfx9pHpgsBRZaHihBaK', // the client id generated for the auth0 app
-    callbackURL: 'https://127.0.0.1:8100', // the base url of the running ionic application.
+    callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application.
   }
 };
